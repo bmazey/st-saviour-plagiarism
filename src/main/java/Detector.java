1b6f1ded4dbf88fbac3faa2928c80327a48902aa
brandon mazey <brandon@brandons-MacBook-Air.local>
2026-02-16 10:23:04 -0500

    looking good

--- src/main/java/Detector.java
+++ src/main/java/Detector.java
@@ -1,5 +1,21 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Detector {
     public static void main(String[] args) {
-        System.out.println("new dawn, new day");
+        // Read the input file and iterate over the contents line-by-line.
+        try {
+            File file = new File("./resources/poem.txt");
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                System.out.println(scanner.nextLine());
+            }
+   
+            scanner.close();
+  
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } 
     }
 }
